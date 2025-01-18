3eb76cc0babeaf3d18ea93f3d134cbb85e4f348c
CTSDM <156813428+CTSDM@users.noreply.github.com>
2025-01-18 09:48:47 +0900

    Remove unnecesary useEffect

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,16 +1,15 @@
 import routes from "../routes";
-import { useEffect, useContext } from "react";
+import { useContext } from "react";
 import { Context as GlobalContext } from "../utils/GlobalStateContext.js";
 
 function Root() {
     const [isLogged] = useContext(GlobalContext);
-    useEffect(() => {
-        if (isLogged) {
-            routes.navigate("/dashboard");
-        } else {
-            routes.navigate("/login");
-        }
-    });
+
+    if (isLogged) {
+        routes.navigate("/dashboard");
+    } else {
+        routes.navigate("/login");
+    }
 
     return null;
 }
